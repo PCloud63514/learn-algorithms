182b7f2db82bb212318a20126a66ba165146a7fb
PCloud63514 <pcloud63514@gmail.com>
2021-02-17 22:58:06 +0900

    풀이 완료

--- 설탕 배달/설탕배달.py
+++ 설탕 배달/설탕배달.py
@@ -1,18 +1,14 @@
 import sys
-number = int(sys.stdin.readline().rstrip())
-A = 3
-B = 5
-n = 1
-i = 0
+number =  int(sys.stdin.readline())
+answer = 0
 
 while True:
-	result1 = (A * n) + (B * i)
-	result2 = (A * i) + (B * n)
-	if (result1 % number == 0) or (result2 % number == 0):
-		print(n + i)
-		break
-	elif (result1 > number) or (result2 > number):
-		i = 0
-		n += 1
-	else:
-		i += 1
+    if number % 5 == 0:
+        answer += int(number / 5)
+        print(answer)
+        break
+    elif number <= 0:
+        print(-1)
+        break
+    number = number - 3
+    answer += 1
