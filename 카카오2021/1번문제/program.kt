6094c9009e4bda281ab6d8b42e55b2a0c8dc6a03
PCloud63514 <PCloud63514@gmail.com>
2021-05-12 23:38:58 +0900

    kakao

--- 카카오2021/1번문제/program.kt
+++ 카카오2021/1번문제/program.kt
@@ -1,7 +1,12 @@
 fun main() {
-    var str:String = "...!@BaT#*..y.abcdefghijklm"
+    println("Start")
+    var str:String = "...!@BaT#*..y.abcdefghijklm."
     str = str.toLowerCase()
     str = str.replace("[^a-z0-9._-]", "")
 
+    while(str.indexOf("..") != -1) {
+        str = str.replace("..", ".")
+    }
+    str = str.replace("^.|.$","")
     println(str)
 }
