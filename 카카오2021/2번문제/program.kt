60af2343e026dd8c678caa0a4bf027e202fd5911
PCloud <pcloud63514@gmail.com>
2021-05-14 14:16:10 +0900

    Wx , XW 합치기만 구현하면 끝

--- 카카오2021/2번문제/program.kt
+++ 카카오2021/2번문제/program.kt
@@ -1,49 +1,67 @@
+// countMap.toList().sortedWith(compareBy({it.second})).toMap() 정렬 코드
+
+// WX 랑 XW 안합쳐짐
 fun main() {
-    val regex = """\W+""".toRegex()
+    var list : Array<String> = arrayOf("XYZ", "XWY", "WXA")
+    var course = arrayListOf(2, 3, 4)
+    var answer:ArrayList<String> = ArrayList<String>()
+    
+    course.forEach { c ->
+        var countMap:MutableMap<String,Int> = HashMap<String, Int>()
+        for(i in 0..list.lastIndex) {
+            // 길이가 c인 녀석들만 다 꺼냄
+            var mutableList = combination(list.get(i), c)
+            
+            // 중첩 계산 함
+            mutableList.forEach { key ->
+                if(countMap.get(key) == null) {
+                    countMap.set(key, 1)
+                } else {
+                    countMap.set(key, countMap.get(key)!! + 1)
+                }
+            }
+        }
+        countMap.forEach {
+            println("key:" + it.key + "| value:" + it.value)
+        }
 
-    val list : List<String> = regex.split("abc abc abc abc")
-
-    list.forEach {
-        println(it)
+        // 이 중 가장 중첩이 많이 된 녀석의 key를 answer에 추가함
+        val maxValue = countMap.values.fold(0, { max, next ->
+            if(max < next) next else max
+        })
+        if(maxValue > 1) {
+            countMap.filter {entriy -> entriy.value == maxValue }.forEach {
+                answer.add(it.key)
+            }
+        }
     }
-    // for(i in 1..list.size) {
-    //     var mutableList:MutableList<List<String>> = mutableListOf<List<String>>()
-        
-    //     combination(mutableList, list, i, Array<Boolean>(list.size) { false })
-
-    //     mutableList.forEach { 
-    //         println(it)
-    //     }
-    // }
-
-    // val answer = solution(arrayOf<String>("ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"), intArrayOf(2,3,4))
-    // println(answer)
+    answer.sort()
+    println(answer.toString())
 }
 
-fun combination(list:MutableList<List<String>>, combineList:List<String>, size:Int, checkB:Array<Boolean>, start:Int=0) {
+fun getSplitList(target:String):List<String> {
+    var list:List<String> = target.split("")
+    return list.subList(1, list.lastIndex)
+}
+
+fun combination(target:String, size:Int):MutableList<String> {
+    var mutableList:MutableList<String> = mutableListOf<String>()
+    var combineList = getSplitList(target)
+
+    _combination(mutableList, combineList, Array<Boolean>(combineList.size){false}, size, 0)
+    
+    return mutableList
+}
+
+fun _combination(cahceList:MutableList<String>, combineList:List<String>, checkB:Array<Boolean>, size:Int, start:Int=0) {
     if(size == 0) {
-        list.addAll( listOf(combineList.filterIndexed { index, _ -> checkB[index] }) )
+        val str = combineList.filterIndexed { index, _ -> checkB[index] }.joinToString("","","")
+        cahceList.add(str)
     } else {
         for(i in start..combineList.lastIndex) {
             checkB[i] = true
-            combination(list, combineList, size-1, checkB, start + 1)
+            _combination(cahceList, combineList, checkB, size-1, i + 1)
             checkB[i] = false
         }
     }
 }
-
-
-
-// fun solution(orders: Array<String>, course: IntArray): Array<String> {
-//     var answer: Array<String> = arrayOf<String>()
-
-//     for(i in 0..orders.lastIndex) {
-//         var arr:List<String> = orders.get(i).split("")
-        
-//         arr.forEach { 
-
-//         }
-
-//     }
-//     return answer
-// }
