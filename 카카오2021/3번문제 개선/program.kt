0cd9c0da73c8fc0c44631ec2ea83bb03579c7f59
PCloud63514 <PCloud63514@gmail.com>
2021-05-17 00:38:24 +0900

    비교도 안될 정도로 빠른 통과 속도인데 왜 안되는 것?
    테스트 1 〉	통과 (10.37ms, 55.5MB)
    테스트 2 〉	통과 (10.69ms, 55.6MB)
    테스트 3 〉	통과 (14.90ms, 55.8MB)
    테스트 4 〉	통과 (25.19ms, 59.1MB)
    테스트 5 〉	통과 (37.00ms, 57.5MB)
    테스트 6 〉	통과 (38.85ms, 58.3MB)
    테스트 7 〉	통과 (48.62ms, 59.1MB)
    테스트 8 〉	통과 (132.24ms, 77MB)
    테스트 9 〉	통과 (134.16ms, 69.6MB)
    테스트 10 〉	통과 (116.33ms, 76.1MB)
    테스트 11 〉	통과 (33.63ms, 59.8MB)
    테스트 12 〉	통과 (39.66ms, 59.4MB)
    테스트 13 〉	통과 (37.20ms, 60MB)
    테스트 14 〉	통과 (100.71ms, 64.1MB)
    테스트 15 〉	통과 (93.57ms, 65.1MB)
    테스트 16 〉	통과 (38.54ms, 57.8MB)
    테스트 17 〉	통과 (43.17ms, 59.4MB)
    테스트 18 〉	통과 (85.24ms, 64.3MB)

--- 카카오2021/3번문제 개선/program.kt
+++ 카카오2021/3번문제 개선/program.kt
@@ -1,37 +1,60 @@
 import kotlin.system.*
 
 fun main() {
-    for(i in 1..(1 shl 4)) {
-        println(i)
-    }
-    // var info:Array<String> = arrayOf<String>("java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50")
-    // var query:Array<String> = arrayOf<String>("java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150")
+    var info:Array<String> = arrayOf<String>("java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50")
+    var query:Array<String> = arrayOf<String>("java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150")
 
-    // val startTime:Long = measureTimeMillis  {
-    //     Solutoin_kakao3().solution(info, query)
-    // }
-    // println(startTime)
+    val startTime:Long = measureTimeMillis  {
+        Kakao2021Question3().solution(info, query)
+    }
+    println(startTime)
 }
 
 class Kakao2021Question3 {
     fun solution(info:Array<String>, query:Array<String>):IntArray {
         var answer:IntArray = IntArray(query.size) { 0 }
         var wordMap:MutableMap<String, Int> = SettingWordMap()
-
-        val regex = " ".toRegex()
+        var scoreList:MutableList<MutableList<Int>> = MutableList<MutableList<Int>>(4*3*3*3) { mutableListOf() }
+        val infoRegex = " ".toRegex()
+        val queryRegex = "( and )|( )".toRegex()
 
         info.forEach {
-            val (infoList, score) = SplitInfo(it, regex)
-            val arr:IntArray =   intArrayOf(
-                wordMap.get(infoList.get(0))!!,
-                wordMap.get(infoList.get(1))!!,
-                wordMap.get(infoList.get(2))!!,
+            val (infoList, score) = SplitInfo(it, infoRegex)
+            val arr:IntArray = intArrayOf(
+                wordMap.get(infoList.get(0))!! * 3 * 3 * 3,
+                wordMap.get(infoList.get(1))!! * 3 * 3,
+                wordMap.get(infoList.get(2))!! * 3,
                 wordMap.get(infoList.get(3))!!)
 
-
-            
+            for(i in  0 until (1 shl 4)) {
+                var index:Int = 0
+                for(j in 0 until 4) {
+                    if(i and (1 shl j) != 0) {
+                        index += arr[j]
+                    }
+                }
+                scoreList.get(index).add(score)
+            }
         }
 
+        scoreList.forEach {
+            it.sort()
+        }
+
+
+        for(i in query.indices) {
+            val(queryInfo, score) = SplitInfo(query.get(i), queryRegex)
+            val index:Int = 
+                (wordMap.get(queryInfo.get(0))!!  * 3 * 3 * 3 +
+                wordMap.get(queryInfo.get(1))!!  * 3 * 3 +
+                wordMap.get(queryInfo.get(2))!!  * 3 +
+                wordMap.get(queryInfo.get(3))!!)
+            answer.set(i, scoreList.get(index).filter { it >= score}.size)
+        }
+
+        for (i in answer.indices) {
+            println(answer.get(i))
+        }
         return answer
     }
 
@@ -43,7 +66,7 @@ class Kakao2021Question3 {
         wordMap.put("python",       3)
         wordMap.put("backend",      1)
         wordMap.put("frontend",     2)
-        wordMap.put("jonior",       1)
+        wordMap.put("junior",       1)
         wordMap.put("senior",       2)
         wordMap.put("chicken",      1)
         wordMap.put("pizza",        2)
@@ -54,6 +77,6 @@ class Kakao2021Question3 {
     fun SplitInfo(info:String, regex:Regex):Pair<List<String>, Int> {
         var infoList:List<String> = info.split(regex)
         val score = infoList.get(4).toInt()
-        return Pair(infoList.subList(0, 3), score)
+        return Pair(infoList.subList(0, 4), score)
     }
 }
