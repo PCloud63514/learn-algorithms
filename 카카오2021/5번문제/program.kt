64f282485ff18f7a2ee09d4048381bf367a03fb7
PCloud63514 <PCloud63514@gmail.com>
2021-05-23 23:23:38 +0900

    풀이 끝

--- 카카오2021/5번문제/program.kt
+++ 카카오2021/5번문제/program.kt
@@ -25,53 +25,51 @@ fun main() {
     output
         - result:String = "01:30:59" */
 class Kakao2021Question5 {
-    /*
-    val ptimeSec = ConvertSecond(play_time)
-        val atimeSec = ConvertSecond(adv_time)
 
-        val logsStartSec:Array<Int> = Array<Int>(logs.size) { 0 }
-        val logsEndSec:Array<Int> = Array<Int>(logs.size) { 0 }
-        val totalTime:Array<Int> = Array<Int>(logs.size) { 0 }
-        
-        for(i in logs.indices) {
-            val slist = logs.get(i).split("-")
-            val sSec = ConvertSecond(slist[0])
-            val eSec = ConvertSecond(slist[1])
-
-            logsStartSec[i] = sSec
-            logsEndSec[i] = eSec
-        }
-         */
     fun solution(play_time: String, adv_time: String, logs: Array<String>): String {
-        var answer: String = ""
         val ptimeSec = ConvertSecond(play_time)
         val atimeSec = ConvertSecond(adv_time)
 
-        val chaceArray:Array<Int> = Array<Int>(ptimeSec) { 0 }
+        val chaceArray:Array<Int> = Array<Int>(ptimeSec + 1) { 0 } //play_time의 총 길이만큼 공간 생성
 
         for( i in logs.indices) {
-            val slist = logs.get(i).split("-")
+            val slist = logs.get(i).split("-") //logs의 시간 분리
+            val sSec= ConvertSecond(slist[0])  //시작 시간의 초
+            val eSec= ConvertSecond(slist[1])  //종료 시간의 초
 
-            val sSec= ConvertSecond(slist[0])
-            val eSec= ConvertSecond(slist[1])
-
-            println("sSec:" + sSec)
-            println("eSec:" + eSec)
-            chaceArray.set(sSec, chaceArray.get(sSec) + 1)
-            chaceArray.set(eSec, chaceArray.get(eSec) - 1)
+            chaceArray.set(sSec, chaceArray.get(sSec) + 1) // 시작 시간 기록
+            chaceArray.set(eSec, chaceArray.get(eSec) - 1) // 종료 시간 기록
         }
 
+        // 이전 index 시간과 현재 index 시간을 더한다.
+        // 1이상이면 상영 중인 것이며, -1을 마주하면 종료한 형식이 된다. 
         for (i in 1 until ptimeSec) {
             chaceArray.set(i, chaceArray.get(i) + chaceArray.get(i - 1))
         }
 
-        for(i in chaceArray.indices) {
-            if(chaceArray.get(i) != 0) {
-                println("i:" + i + "= " + chaceArray.get(i))
+        var answerNum:Int = 0
+        var answerIndex:Int = atimeSec
+        
+        for(i in 1..atimeSec) {
+            answerNum += chaceArray.get(i)
+        }
+        var tempNum = answerNum
+
+        for(i in (atimeSec + 1)..ptimeSec) {
+            answerNum = answerNum + chaceArray.get(i) - chaceArray.get(i-atimeSec)
+            if (answerNum > tempNum) {
+                tempNum = answerNum
+                answerIndex = i - atimeSec + 1
             }
         }
 
+        var answer = String.format("%02d:%02d:%02d", answerIndex / 3600, answerIndex / 60 % 60, answerIndex % 60)
+        
         return answer
+    }
+
+    fun Max(A:Int, B:Int):Int {
+        return if( A <= B) B else A
     }
 
     fun ConvertSecond(strTime:String):Int {
