8ff841b9908512b5cdae4768ae203414edb9d2aa
PCloud63514 <PCloud63514@gmail.com>
2021-05-25 01:07:02 +0900

    memo

--- 카카오2021/6번문제/program.kt
+++ 카카오2021/6번문제/program.kt
@@ -9,6 +9,9 @@ fun main() {
 
     println(Kakao2021Question6().solution(board, 1, 0))
 }
+
+// 4x4 카드 뒤집기
+// 처음 어떤 카드를 뒤집을지만 정하면 계산에는 문제 없을 것 같다
 class Kakao2021Question6 {
     fun solution(board: Array<IntArray>, r: Int, c: Int): Int {
         var answer: Int = 0
