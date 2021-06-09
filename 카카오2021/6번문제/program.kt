5a8bc11843481d33721ca8b08ffa90314da8d8b7
PCloud63514 <PCloud63514@gmail.com>
2021-06-10 02:13:00 +0900

    해답 참고함

---
+++ 카카오2021/6번문제/program.kt
@@ -0,0 +1,11 @@
+fun main() {
+    
+    Kakao2021Question6().solution(board, r, c);
+}
+
+class Kakao2021Question6 {
+    fun solution(board: Array<IntArray>, r: Int, c: Int): Int {
+        var answer: Int = 0
+        return answer
+    }
+}
