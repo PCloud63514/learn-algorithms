8346ce86af9ee7350bb8346da120dc22ae18155b
PCloud <pcloud63514@gmail.com>
2021-07-29 19:32:58 +0900

    fix

--- 카카오/2020/기둥과 보 설치.java
+++ 카카오/2020/기둥과 보 설치.java
@@ -1,19 +1,15 @@
-package com.pcloud.simplespringsecurity;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.parameters.P;
-
-@SpringBootTest
 class Solution {
-    int cnt = 0; // 설치된 구조물의 갯수
     int X = 0;
     int Y = 1;
     int TYPE = 2;
     int COMMAND = 3;
     int TYPE_PILLAR = 0;
-    int TYPE_BOARD = 1;
+    int TYPE_PLATE = 1;
     int COMMAND_REMOVE = 0;
     int COMMAND_BUILD = 1;
+
+    boolean[][] box_pillar;
+    boolean[][] box_plate;
     /**
      *
      * @param n 벽면의 크기
@@ -27,80 +23,96 @@ class Solution {
      *                    바닥에 보를 설치 하는 경우는 없습니다.
      * @return
      */
-    @Test
     public int[][] solution(int n, int[][] build_frame) {
-        int[][] box = new int[n][n]; // 건축물이 설치될 공간. (빈공간 표시를 위해 box에선 기둥 1 보 2이다.
+        box_pillar = new boolean[n + 2][n + 2]; // out of range를 피하기 위해 패딩을 줌
+        box_plate = new boolean[n + 2][n + 2];  // out of range를 피하기 위해 패딩을 줌
+        int cnt = 0; // 설치된 구조물의 갯수
 
-        for (int i = 0; i < build_frame.length; ++i) {
-            if (build_frame[i][COMMAND] == COMMAND_BUILD) {
-                build(box, build_frame[i][X], build_frame[i][Y], build_frame[i][TYPE]);
+        for (int[] build : build_frame) {
+            if (build[TYPE] == TYPE_PILLAR) {
+                if (build[COMMAND] == 1) {
+                    if (checkPillar(build[X], build[Y])) {
+                        box_pillar[build[X]][build[Y]] = true;
+                        cnt++;
+                    }
+                } else {
+                    // 삭제
+                    box_pillar[build[X]][build[Y]] = false;
+                    if (canDelete(build[X], build[Y]) == false) {
+                        box_pillar[build[X]][build[Y]] = true;
+                    } else {
+                        cnt--;
+                    }
+                }
             } else {
-
-            }
-        }
-
-        int[][] answer = new int[cnt][3];
-        for (int i = 0; i < cnt; ++i) {
-            for (int row = 0; row < n; ++row) {
-                for (int col = 0; col < n; ++col) {
-                    // box 값이 0이 아닌 값은 건축물
-                    if (box[row][col] != 0) {
-                        answer[i][X] = row;
-                        answer[i][Y] = col;
-                        answer[i][TYPE] = box[row][col] - 1;
+                if (build[COMMAND] == 1) {
+                    if (checkPlate(build[X], build[Y])) {
+                        box_plate[build[X]][build[Y]] = true;
+                        cnt++;
+                    }
+                } else {
+                    box_plate[build[X]][build[Y]] = false;
+                    if (canDelete(build[X], build[Y]) == false) {
+                        box_plate[build[X]][build[Y]] = true;
+                    } else {
+                        cnt--;
                     }
                 }
             }
         }
+        int[][] answer = new int[cnt][3];
+        cnt = 0;
+        for (int x = 0; x <= n; ++x) {
+            for (int y = 0; y <= n; ++y) {
+                if (box_pillar[x][y]) {
+                    answer[cnt][X] = x;
+                    answer[cnt][Y] = y;
+                    answer[cnt++][TYPE] = TYPE_PILLAR;
+                }
 
+                if (box_plate[x][y]) {
+                    answer[cnt][X] = x;
+                    answer[cnt][Y] = y;
+                    answer[cnt++][TYPE] = TYPE_PLATE;
+                }
+            }
+        }
         return answer;
     }
 
-    /**
-     * 기둥 설치 기준
-     *  - x이 0 일 경우
-     *  - x가 1 이상이며, x - 1에 구조물이 설치되었을 때 (값이 0이 아닐 때)
-     * 보 설치 기준
-     *  - x가 1이상이며, 아래 기둥이 설치되었을 때
-     *  - x가 1이상이며, 양 옆에 보가 설치되어 있을 때
-     *
-     * @param box
-     * @param x
-     * @param y
-     * @param type
-     */
-    private void build(int[][] box, int x, int y, int type) {
-        // 기둥 일 때
-        if (type == TYPE_PILLAR) {
-            // 설치할 x가 0일 때 - 바닥
-            if (x == 0) {
-                box[X][Y] = TYPE_PILLAR + 1;
-                cnt++;
-            } else {
-                // 자신 아래에 기둥 or 보가 있을 때
-                if (box[X][Y - 1] != 0) {
-                    box[X][Y] = TYPE_PILLAR;
-                    cnt++;
+    private boolean canDelete(int x, int y) {
+        for (int i = Math.max(x-1, 0); i <= x+1; ++i) {
+            for (int j = y; j <= y+1; ++j) {
+                if (box_pillar[i][j] && checkPillar(i , j) == false) {
+                    return false;
                 }
-            }
-        } else {
-            if (1 <= x) {
-                // 아래가 기둥일 때 
-                if (box[x][y - 1] == TYPE_PILLAR + 1) {
-                    box[x][y] = TYPE_BOARD + 1;
-                    cnt++;
-                    return;
+                if (box_plate[i][j] && checkPlate(i, j) == false) {
+                    return false;
                 }
-                // 양 옆에 보가 설치되었을 때
-                if(0 < y && y < box.length) {
-                    if (box[x][y - 1] == TYPE_BOARD + 1 && box[x][y + 1] == TYPE_BOARD + 1) {
-                        box[x][y] = TYPE_BOARD + 1;
-                        cnt++;
-                    }
-                }
-                
             }
         }
+
+        return true;
+    }
+
+    private boolean checkPlate(int x, int y) {
+        // 판 오른쪽에도 설치가 가능
+        if (box_pillar[x][y-1] || box_pillar[x+1][y-1]) {
+            return true;
+        }
+        if (0 < x && box_plate[x-1][y] && box_plate[x+1][y]) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkPillar(int x, int y) {
+        if (y == 0 || box_pillar[x][y - 1]) {
+            return true;
+        }
+        if ((0 < x && box_plate[x-1][y]) || box_plate[x][y]) {
+            return true;
+        }
+        return false;
     }
 }
-// 그냥 throw 처리 해서 제외시켜야할듯?
