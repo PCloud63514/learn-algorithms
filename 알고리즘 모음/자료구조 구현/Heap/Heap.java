762042fb8c9d730cecef0c57cb479b80dfa7d02b
PCloud63514 <pcloud63514@gmail.com>
2021-12-07 23:03:12 +0900

    update

--- 알고리즘 모음/자료구조 구현/Heap/Heap.java
+++ 알고리즘 모음/자료구조 구현/Heap/Heap.java
@@ -1,10 +1,5 @@
-// https://programmers.co.kr/learn/courses/30/lessons/42626?language=java
-// https://kim6394.tistory.com/222
-// https://st-lab.tistory.com/205
-// https://gmlwjd9405.github.io/2018/05/10/data-structure-heap.html
-// https://ko.wikipedia.org/wiki/%ED%9E%99_(%EC%9E%90%EB%A3%8C_%EA%B5%AC%EC%A1%B0)
-// https://st-lab.tistory.com/243
-// https://twpower.github.io/137-heap-implementation-in-cpp
+import java.util.*;
+
 class Heap<T> {
     private final Comparator<T> _comparator;
     private int _capacity = 10;
@@ -39,16 +34,9 @@ class Heap<T> {
 
         Object node = _nodes[parentIdx];
         swap(parentIdx, _last--);
-        
-        while (childIdx < _last) {
-             if (childIdx + 1 < _last) {
-                 
-             }
-        }
-        
+
         if(childIdx + 1 <= _last) {
-            // 클 경우 오른쪽 트리의 노드가
-            childIdx = _comparator.compare((T)_nodes[childIdx], (T)_nodes[childIdx + 1]) == 1 ? childIdx : childIdx + 1;
+            childIdx = _comparator.compare((T)_nodes[childIdx], (T)_nodes[childIdx + 1]) != 1 ? childIdx : childIdx + 1;
         }
 
         while (childIdx <= _last && 1 == _comparator.compare((T) _nodes[parentIdx], (T) _nodes[childIdx])) {
@@ -57,11 +45,9 @@ class Heap<T> {
             childIdx = childIdx * 2;
 
             if(childIdx + 1 <= _last) {
-                // 클 경우 오른쪽 트리의 노드가
-                childIdx = _comparator.compare((T)_nodes[childIdx], (T)_nodes[childIdx + 1]) == 1 ? childIdx : childIdx + 1;
+                childIdx = _comparator.compare((T)_nodes[childIdx], (T)_nodes[childIdx + 1]) != 1 ? childIdx : childIdx + 1;
             }
         }
-
         return (T)node;
     }
 
@@ -84,13 +70,23 @@ class Heap<T> {
     public int getSize() {
         return this._capacity;
     }
-}
 
-class UpComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer i1, Integer i2) {
-        if (i1 > i2)
-            return 1;
-        return 0;
+    static class UpComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer i1, Integer i2) {
+            if (i1 > i2)
+                return 1;
+            return 0;
+        }
+    }
+
+    static class DownComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer i1, Integer i2) {
+            if (i1 < i2)
+                return 1;
+            return 0;
+        }
     }
 }
