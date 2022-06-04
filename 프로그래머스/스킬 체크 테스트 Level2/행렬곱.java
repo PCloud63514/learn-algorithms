520928d662f4d90cf13aebb445910f9d5d38ce0b
PCloud <pcloud63514@gmail.com>
2022-06-04 23:08:14 +0900

    문제 풀이 추가

---
+++ 프로그래머스/스킬 체크 테스트 Level2/행렬곱.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int x = 0; x < answer[0].length; x++) {
+                int num = 0;
+                for (int y = 0; y < arr2.length; y++) {
+                    num += arr1[i][y] * arr2[y][x];
+                }
+                answer[i][x] = num;
+            }
+        }
+        return answer;
+    }
+}
