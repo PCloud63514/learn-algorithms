c39671a20ae359b5797a7491b02d3ca4614496c1
PCloud <pcloud63514@gmail.com>
2022-10-18 21:34:55 +0900

    문제 풀이 추가

--- 프로그래머스/스킬 체크 테스트 Level2/최솟값 만들기.java
+++ 프로그래머스/스킬 체크 테스트 Level2/최솟값 만들기.java
@@ -4,19 +4,13 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-
-        int[] aSorted = Arrays.stream(A)
-                .sorted()
-                .toArray();
-
-        int[] bSorted = Arrays.stream(B)
-                .sorted()
-                .toArray();
-
         int endPoint = A.length - 1;
 
+        Arrays.sort(A);
+        Arrays.sort(B);
+
         for (int i = 0; i < A.length; i++) {
-            answer += aSorted[i] * bSorted[endPoint - i];
+            answer += A[i] * B[endPoint - i];
         }
 
         return answer;
